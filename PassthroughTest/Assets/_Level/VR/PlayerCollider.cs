c7ef22a4502487326e17238d34e77b1250b75aaa
Jingyuan_Li <98413933+LeaLeeNyu@users.noreply.github.com>
2023-03-31 15:34:45 -0400

    Level 0 Update

---
+++ PassthroughTest/Assets/_Level/VR/PlayerCollider.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollider : MonoBehaviour
+{
+    [SerializeField] private Camera mainCamera;
+
+    private void Start()
+    {
+        transform.rotation = Quaternion.Euler(transform.rotation.x, mainCamera.transform.eulerAngles.y, transform.rotation.z);
+    }
+
+    private void Update()
+    {
+        transform.position = new Vector3(mainCamera.transform.position.x, transform.position.y, mainCamera.transform.position.z);
+        //transform.rotation = Quaternion.Euler(transform.rotation.x, mainCamera.transform.eulerAngles.y, transform.rotation.z);
+    }
+}
