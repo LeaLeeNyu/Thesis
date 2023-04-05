d00a4f8104bb46ac8376c94a37361259f2b03b6d
Jingyuan_Li <jl11435jl11435@gmail.com>
2023-04-04 22:10:57 -0400

    Level0_0 update

---
+++ PassthroughTest/Assets/_Level/Level_0/Script/SideSwitch.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SideSwitch : MonoBehaviour
+{
+    [SerializeField] private GameObject ying;
+    [SerializeField] private GameObject yang;
+
+    private bool yingVisability = false;
+    private bool yangVisability = true;
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            yingVisability = !yingVisability;
+            yangVisability = !yangVisability;
+            ying.SetActive(yingVisability);
+            yang.SetActive(yangVisability);
+        }
+    }
+}
